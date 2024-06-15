743ef29f99d4574bf59e124a4d0ef03ec0fcb8bd
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-06-15 11:37:43 +0300

    catalog- range radio checkbox

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,4 @@
+// burger
 window.addEventListener('load', () => {
   const button = document.querySelector('.js-toggle-button');
   const navMenu = document.querySelector('.js-nav-menu');
@@ -18,45 +19,46 @@ window.addEventListener('load', () => {
   });
 });
 
-const rangeInput = document.querySelectorAll('.range-input input');
-const priceInput = document.querySelectorAll('.price-input input');
-const range = document.querySelector('.slider .progress');
 
-const priceGap = 1000;
+// range
+const rangeSliderInit = () => { // создаем функцию инициализации слайдера
+  const range = document.querySelector('.range'); // Ищем слайдер
+  const inputMin = document.querySelector('.filter__range-min'); // Ищем input с меньшим значнием
+  const inputMax = document.querySelector('.filter__range-max'); // Ищем input с большим значнием
 
-priceInput.forEach((input) => {
-  input.addEventListener('input', (e) => {
-    const minPrice = parseInt(priceInput[0].value);
-    const maxPrice = parseInt(priceInput[1].value);
+  if (!range || !inputMin || !inputMax) {
+    return;
+  } // если этих элементов нет, прекращаем выполнение функции, чтобы не было ошибок
 
-    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
-      if (e.target.className === 'input-min') {
-        rangeInput[0].value = minPrice;
-        range.style.left = `${(minPrice / rangeInput[0].max) * 100 }%`;
-      } else {
-        rangeInput[1].value = maxPrice;
-        range.style.right = `${100 - (maxPrice / rangeInput[1].max) * 100 }%`;
-      }
-    }
+  const inputs = [inputMin, inputMax]; // создаем массив из меньшего и большего значения
+
+  noUiSlider.create(range, { // инициализируем слайдер
+    start: [0, 900], // устанавливаем начальные значения
+    connect: true, // указываем что нужно показывать выбранный диапазон
+    range: { // устанавливаем минимальное и максимальное значения
+      'min': 0,
+      'max': 1000
+    },
+    step: 1, // шаг изменения значений
+  }
+  );
+
+  range.noUiSlider.on('update', (values, handle) => { // при изменений положения элементов управления слайдера изменяем соответствующие значения
+    inputs[handle].value = parseInt(values[handle], 10);
   });
-});
 
-rangeInput.forEach((input) => {
-  input.addEventListener('input', (e) => {
-    const minVal = parseInt(rangeInput[0].value);
-    const maxVal = parseInt(rangeInput[1].value);
-
-    if (maxVal - minVal < priceGap) {
-      if (e.target.className === 'range-min') {
-        rangeInput[0].value = maxVal - priceGap;
-      } else {
-        rangeInput[1].value = minVal + priceGap;
-      }
-    } else {
-      priceInput[0].value = minVal;
-      priceInput[1].value = maxVal;
-      range.style.left = `${(minVal / rangeInput[0].max) * 100 }%`;
-      range.style.right = `${100 - (maxVal / rangeInput[1].max) * 100 }%`;
-    }
+  inputMin.addEventListener('change', function () { // при изменении меньшего значения в input - меняем положение соответствующего элемента управления
+    range.noUiSlider.set([this.value, null]);
   });
-});
+
+  inputMax.addEventListener('change', function () { // при изменении большего значения в input - меняем положение соответствующего элемента управления
+    range.noUiSlider.set([null, this.value]);
+  });
+
+};
+
+const init = () => {
+  rangeSliderInit(); // запускаем функцию инициализации слайдера
+};
+
+window.addEventListener('DOMContentLoaded', init); // запускаем функцию init, когда документ будет загружен и готов к взаимодействию
